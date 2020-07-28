9694d8e9d39e2bed1deca06c80993260a12f8a2b
Nyannsuke01 <shingo_373_100_10ve@yahoo.co.jp>
2020-07-28 23:38:59 +0900

    再生/停止ボタンのリファクタリングを行いました。

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,12 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var slideImageView: UIImageView!
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var backButton: UIButton!
     @IBOutlet weak var playAndPauseButton: UIButton!
-
+    
     //画像を配列に格納
     var imagePictureArray: Array = ["0","1","2","3","4","5"]
     //imageViewのカウンター
@@ -31,13 +31,15 @@ class ViewController: UIViewController {
         //画像の取り込み
         let image = UIImage(named: "0")
         slideImageView.image = image
-
+        //再生ボタンとしてセット
+        playAndPauseButton.setTitle("再生", for: .normal)
     }
-
+    
     @objc func updateTimer(_ timer: Timer) {
+        //計測用
         self.timer_sec += 2.0
         print(timer_sec)
-        //冗長化を防ぎたい
+        //次の画像に移る　最後の画像の次は最初の画像に戻る。
         if imagePictureCount < imagePictureArray.count - 1 {
             imagePictureCount += 1
             changeImage()
@@ -46,19 +48,20 @@ class ViewController: UIViewController {
             changeImage()
         }
     }
-
+    
     func changeImage() {
         let name = imagePictureArray[imagePictureCount]
         let image = UIImage(named: name)
         slideImageView.image = image
     }
-
+    
     @IBAction func tapImage(_ sender: Any) {
-        print("画像がタップされました")
         performSegue(withIdentifier: "showPicture", sender: nil)
+        stopSlide()
     }
-
+    
     @IBAction func goNext(_ sender: Any) {
+        //次の画像に移る　最後の画像で押されたら、最初の画像に戻る
         if imagePictureCount < imagePictureArray.count - 1 {
             imagePictureCount += 1
             changeImage()
@@ -67,9 +70,9 @@ class ViewController: UIViewController {
             changeImage()
         }
     }
-
+    
     @IBAction func goBack(_ sender: Any) {
-        //配列の個数のカウント
+        //前のスライドに戻る　最初の画像で押されたら、最後の画像に移る
         if imagePictureCount > 0 {
             imagePictureCount -= 1
             changeImage()
@@ -78,27 +81,43 @@ class ViewController: UIViewController {
             changeImage()
         }
     }
-
+    
     @IBAction func playAndPause(_ sender: Any) {
-
+        //再生停止フラグ 停止:「0」中であれば再生、再生:「1」中であれば停止
         if playAndPauseFlag == 0 {
-            //自動送りの間は、進むボタンと戻るボタンはタップ不可にしてください
-            nextButton.isEnabled = false
-            backButton.isEnabled = false
-            //再生停止フラグ 停止:「0」再生:「1」
-            playAndPauseFlag = 1
-
-            self.timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(updateTimer(_:)), userInfo: nil, repeats: true)
+            playSlide()
         } else {
-            //自動送りの間は、進むボタンと戻るボタンはタップ不可にしてください
-            nextButton.isEnabled = true
-            backButton.isEnabled = true
-            //再生停止フラグ 停止:「0」再生:「1」
-            playAndPauseFlag = 0
+            stopSlide()
+        }
+    }
+    
+    func playSlide() {
+        //進むボタンと戻るボタンを非活性
+        nextButton.isEnabled = false
+        backButton.isEnabled = false
+        //再生停止フラグ 停止:「0」再生:「1」
+        playAndPauseFlag = 1
+        //停止ボタンにする
+        playAndPauseButton.setTitle("停止", for: .normal)
+        //タイマーを２秒毎にセット
+        self.timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(updateTimer(_:)), userInfo: nil, repeats: true)
+    }
+    
+    func stopSlide() {
+        //進むボタンと戻るボタンを活性化
+        nextButton.isEnabled = true
+        backButton.isEnabled = true
+        //再生停止フラグ 停止:「0」再生:「1」
+        playAndPauseFlag = 0
+        //再生ボタンにする
+        playAndPauseButton.setTitle("再生", for: .normal)
+        //タイマーが動いていたら
+        if self.timer != nil {
             //タイマー停止
             self.timer.invalidate()
         }
     }
+    
     //遷移と共に画像を渡す
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let showPictureVC = segue.destination as? ShowPictureViewController
